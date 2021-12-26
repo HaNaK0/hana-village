6e0edfa198aaba31f4d88f56e4c626e8b19c3b1e
HaNaK0 <hampus@hampushuledal.com>
2021-12-26 21:27:37 +0100

    Add fade to log text

--- src/logger.lua
+++ src/logger.lua
@@ -1,5 +1,5 @@
 local logger = {
-    log_time = 2,
+    log_time = 3,
     log_origin = { x = 0, y = 0},
     text_size = 12,
     font = love.graphics.setNewFont(12),
@@ -8,6 +8,15 @@ local logger = {
 
 function logger:log(message)
     table.insert(self.logs, {message = message, time = 0})
+end
+
+local function intensity(t)
+    -- This function controlls intesity
+    -- t is the time from 0 to 1
+    -- the function returns an intensity value from 0 to 1
+    local unclampedOut = -3 * t + 3
+
+    return math.min(unclampedOut, 1)
 end
 
 function logger:update(dt)
@@ -30,6 +39,7 @@ function logger:draw()
     local x = self.log_origin.x
 
     for index, value in ipairs(self.logs) do
+        love.graphics.setColor(1,1,1, intensity(value.time / self.log_time))
         local y = self.log_origin.y + (self.font:getAscent() * (index - 1))
         love.graphics.print(value.message, x, y)
     end
